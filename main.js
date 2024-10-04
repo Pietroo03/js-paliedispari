c5866f0c53b6e88ebf20ff9c4a409a4b1f5c574a
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-04 17:05:23 +0200

    modified comments

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 - Chiedere all’utente di inserire una parola 
 - Creare una funzione per capire se la parola inserita è palindroma */
 
-/* function palindrome_word() {
+function palindrome_word() {
     let word = prompt('Inserisci la tua parola')
     console.log(word);
     let splitWord = word.split('')
@@ -19,7 +19,7 @@
 }
     
 palindrome_word()
- */
+
 
 
 
